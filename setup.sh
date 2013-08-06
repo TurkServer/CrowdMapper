ed96b6805ba35abd87fee79610d64bf0b6742b92
Andrew Mao <mizzao@gmail.com>
2013-08-06 16:21:18 -0400

    don't store minified OpenLayers in repo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-curl -L http://dev.openlayers.org/releases/OpenLayers-2.12/OpenLayers.js > client/compatibility/OpenLayers.js
+#!/bin/bash
+curl -L http://dev.openlayers.org/releases/OpenLayers-2.13.1/OpenLayers.js > client/compatibility/OpenLayers.js
 # below not needed since in client/compatibility
 # echo 'window.OpenLayers = OpenLayers;' >> client/lib/OpenLayers.js
