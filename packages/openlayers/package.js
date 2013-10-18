2fdec85d482355261515baad501a17b4477340ea
Andrew Mao <mizzao@gmail.com>
2013-10-18 01:07:36 -0400

    test tutorial spotlight and update to 0.6.6.1

--- packages/openlayers/package.js
+++ packages/openlayers/package.js
@@ -1,11 +1,9 @@
 Npm.depends({
-    connect: "2.7.10"
+    connect: "2.9.0" // Current as of 0.6.6, but find and use handle from WebApp
 });
 
-var both = ['client', 'server'];
-
 Package.on_use(function (api) {
-    api.use('coffeescript', both);
+    api.use('coffeescript');
     api.use(['routepolicy', 'webapp'], 'server');
 
     api.add_files('client.html', 'client');
