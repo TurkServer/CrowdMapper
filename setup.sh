4b03bd3ac8de6751ed0a0e710248eb70175eec29
Andrew Mao <mizzao@gmail.com>
2013-10-21 18:25:54 -0400

    refactor tutorial into a separate package

--- setup.sh
+++ setup.sh
@@ -5,7 +5,3 @@ curl -L http://dev.openlayers.org/releases/OpenLayers-2.13.1/OpenLayers.js > cli
 # below not needed since in client/compatibility
 # echo 'window.OpenLayers = OpenLayers;' >> client/lib/OpenLayers.js
 curl -L http://dev.openlayers.org/releases/OpenLayers-2.13.1/theme/default/style.css > client/compatibility/OpenLayers.css
-
-# jQuery-UI 1.10.3
-curl -L http://code.jquery.com/ui/1.10.3/jquery-ui.min.js > client/compatibility/jquery-ui.min.js
-curl -L http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css > client/compatibility/jquery-ui.css
