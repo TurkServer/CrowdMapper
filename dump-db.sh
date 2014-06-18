1464ffd297124c2142757b43f9c701db19035329
Andrew Mao <mizzao@gmail.com>
2014-06-18 17:13:08 -0400

    Dump db from the correct port

--- dump-db.sh
+++ dump-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf .backups/cmdump
-mongodump --host localhost:3002 -o .backups/cmdump
+mongodump --host localhost:3001 -o .backups/cmdump
 tar cjvf .backups/cmdump.tar.bz2 .backups/cmdump/
