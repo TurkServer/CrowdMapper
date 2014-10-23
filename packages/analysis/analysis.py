27e80253aba80567034aa1623f51312de9381fe8
Andrew Mao <mizzao@gmail.com>
2014-10-23 14:20:55 -0400

    successful rpc to python

---
+++ packages/analysis/analysis.py
@@ -0,0 +1,27 @@
+# RPC server using the method described at
+# http://ianhinsdale.com/code/2013/12/08/communicating-between-nodejs-and-python/
+
+import zerorpc
+
+from munkres import Munkres
+
+class AnalysisRPC(object):
+    def __init__(self):
+        self.m = Munkres()
+
+    def hello(self, name):
+        print "Hello called with: %s" % name
+        return "Hello, %s" % name
+
+    # mat is an nxm list of lists 
+    # representing a weight matching matrix
+    # satisfying 0 < mat(x,y) < 1
+    def maxMatching(self, mat):
+        result = self.m.compute(mat)
+        return sum([1 - mat[row][column] for row, column in result])
+
+s = zerorpc.Server(AnalysisRPC())
+s.bind("tcp://127.0.0.1:4242")
+
+print "Starting RPC server..."
+s.run()
--- packages/analysis/server.coffee
+++ packages/analysis/server.coffee
@@ -8,6 +8,13 @@ client.connect("tcp://127.0.0.1:4242")
 # Print out any errors that might be encountered
 client.on "error", (err) -> console.error("RPC client error: ", err)
 
-invokeSync = Meteor.wrapAsync(client.invoke, client)
+Analysis =
+  invoke: Meteor.wrapAsync(client.invoke, client)
 
-class Analysis
+Meteor.defer ->
+  console.log "Trying python RPC server..."
+  try
+    response = Analysis.invoke("maxMatching", [ [0, 0.5], [1, 0.5] ])
+    console.log("Got python response (expect 1.5): ", response)
+  catch e
+    console.error("RPC error: ", e)
